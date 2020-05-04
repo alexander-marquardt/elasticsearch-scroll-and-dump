a893eec3c3432be0b71318747b15e62512b6e06a
Alexander Marquardt <alexander.marquardt@elastic.co>
2020-05-04 20:42:45 +0100

    Simplified the code by using the helper

--- scroll-and-dump.py
+++ scroll-and-dump.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 import json
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 
 ES_HOST = 'localhost:9200'
 ES_USER = 'elastic'
@@ -10,35 +10,13 @@ OUT_FILE = '/tmp/dump-example.txt'
 es = Elasticsearch([ES_HOST], http_auth=(ES_USER, ES_PASSWORD))
 
 
-def dump_hits(hits, out_file):
-    for hit in hits:
-        out_file.write(f"{json.dumps(hit['_source'])}\n")
-
-
 def scroll_and_dump():
-    
-    data = es.search(index=INDEX_TO_DUMP, scroll='1m', body={"query": {"match_all": {}}})
-
-    # Get the scroll ID
-    sid = data['_scroll_id']
-    scroll_size = len(data['hits']['hits'])
 
     with open(OUT_FILE, 'w') as out_file:
 
         # Before scroll, process current batch of hits
-        dump_hits(data['hits']['hits'], out_file)
-
-        while scroll_size > 0:
-            data = es.scroll(scroll_id=sid, scroll='2m')
-
-            # Process current batch of hits
-            dump_hits(data['hits']['hits'], out_file)
-
-            # Update the scroll ID
-            sid = data['_scroll_id']
-
-            # Get the number of results that returned in the last scroll
-            scroll_size = len(data['hits']['hits'])
+        for data in helpers.scan(es, index=INDEX_TO_DUMP):
+            out_file.write(f"{json.dumps(data['_source'])}\n")
 
 
 if __name__ == "__main__":
